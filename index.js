8a50244f980aa7f3e54ca6cfe17b0ae3460b2622
Aziz Limonu <azizlimonu@gmail.com>
2023-04-13 14:14:36 +0800

    updates

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 
-dbConnect();
+const authRouter = require('./routes/authRoutes');
+const bodyParser = require('body-parser');
 
-app.use('/',(req,res)=>{
-  res.send('Test Api oke')
-});
+dbConnect();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/user', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is Running on PORT ${PORT}`);
